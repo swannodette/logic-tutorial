f7a6d47d77066af01f25db7f1cf615a6bfea2266
David Nolen <david.nolen@gmail.com>
2011-04-29 01:18:40 -0400

    * src/logic_tutorial/tut2.clj: add tut2

---
+++ src/logic_tutorial/tut2.clj
@@ -0,0 +1,11 @@
+(ns logic-tutorial.tut2
+  (:refer-clojure :exlude [reify == inc])
+  (:use [clojure.core.logic minikanren rel]))
+
+(defn appendo [l1 l2 o]
+  (conde
+    ((== l1 ()) (== l2 o))
+    ((exist [a d r]
+       (conso a d l1)
+       (conso a r l2)
+       (appendo d l2 r)))))
