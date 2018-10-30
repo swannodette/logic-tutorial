cda60553cd6ecb5c090e9a6ab4e9492828e8d500
Joe Lane <jplane.tech@gmail.com>
2018-10-30 05:41:24 -0500

    Upgrade dependencies

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject logic-tutorial "1.0.0"
   :description "A Very Gentle Introduction to Relational and Functional Programming"
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/tools.macro "0.1.2"]
-                 [org.clojure/core.logic "0.8.5"]])
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/tools.macro "0.1.5"]
+                 [org.clojure/core.logic "0.8.11"]])
